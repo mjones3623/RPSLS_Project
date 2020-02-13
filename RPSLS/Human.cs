de7f292d3e29c4066b566ce6a8ff5698c0c8a0e9
Mathew Jones <anilox123@gmail.com>
2020-02-13 06:55:44 -0600

    message

--- RPSLS/Human.cs
+++ RPSLS/Human.cs
@@ -18,6 +18,38 @@ namespace RPSLS
                 Console.WriteLine("Choose, and type, either rock, paper, scisors, lizard, or spock");
 
                 gesture = Console.ReadLine();
+
+                if (gesture == "rock")
+                {
+
+                    Console.WriteLine("You entered rock");
+                }
+                else if (gesture == "paper")
+                {
+
+                    Console.WriteLine("You entered paper");
+                }
+                else if (gesture == "scisors")
+                {
+
+                    Console.WriteLine("You entered scisors");
+                }
+                else if (gesture == "lizard")
+                {
+
+                    Console.WriteLine("You entered lizard");
+                }
+                else if (gesture == "spock")
+                {
+
+                    Console.WriteLine("You entered spock");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid option:");
+
+                }
+
                 
                 return gesture;
             }
